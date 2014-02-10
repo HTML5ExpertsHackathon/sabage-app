fa1695dfa7e52ed6c8d409b5a15ebf7937bc7469
Y-NAKA <com.y.naka@gmail.com>
2014-02-11 03:05:28 +0900

    互いの位置情報を交換しあうところまでリファクタリングした

--- sabage.js
+++ sabage.js
@@ -3,70 +3,102 @@
  */
 
 $(document).ready(function(){
+
     locationM = new Location.manager();
-    SkywayM = new SkyWay.manager();
-    locationM.onposition = SkywayM.sendLocation;
-    SkywayM.dcCallback = locationM.setOtherMarker;
-    UI.init();
+    skywayM = new SkyWay.manager();
+    UI = new UI.manager();
 
-    setInterval(function(){
-        var data = locationM.lastlocation;
-        SkywayM.sendLocation(data)
-    }, 1000)
-    
-    //LocationManager
+    var docType = {
+        firstLocation: 1,
+        updateLocation: 2,
+        message: 2
+    }
 
-    //LocationManagerを初期化
+    $('body').on('click', '#title-screen.active', function(){
+        $('.blink').removeClass('blink').addClass('selected');
+        $('#confirm')[0].play();
+        $('#theme').animate({volume:0}, 1000);
+        setTimeout(function(){
+                UI.setScreen($('#login-screen'));
+            }, 1000);
+    });
 
-    //自分の位置を表示開始する
-    // locationM.setMyMarker();
+    $('body').on('click', '#login-button', function(){
+        var team = $('#team-input').val();
+        var player = $('#player-input').val();
+        if(!team.length || !player.length){
+            UI.showError('Login Error', 'Please enter your team name and player name.')
+            return;
+        }
 
+        UI.setScreen($('#radar-screen'),function(){
 
-    // var other = {
-    //     markerObject: null,
-    //     latitude: 36.3005131,
-    //     longitude: 138.8473839
-    // }
+            locationM.init('#gmap');
+            locationM.setMyMarker(function(myLocation){
 
-    //相手の位置を表示する
-    // locationM.setOtherMarker(other);
-    //SkyWayManager
+                $('#start')[0].play();
+                skywayM.init(team,player,function(peer){
 
-    //SkyWayManagerを初期化
-    // SkywayM.init(2,'yusuke3');
+                    myLocation.type = docType.firstLocation;
+                    skywayM.sendMessage(myLocation,peer);
 
-    //接続を開始
-    // SkywayM.setConnectionHandler(function(msg){
-    //     console.log(msg);
-    // });
+                },function(err){
 
-/*    $('#join').on('click',function(e){
-        e.preventDefault();
-        
-        //SkyWayManagerを初期化
-        SkywayM.init(1,$('#id').val());
+                    UI.showError('Login Error', err);
 
-        //接続を開始
-        SkywayM.setConnectionHandler(function(msg){
-            console.log(msg);
-        });
+                },function (peer,msg){
 
-        $('#sendLocation').on('click',function(e){
-            e.preventDefault();
-            SkywayM.sendLocation('test');
-        });
+                    if(msg.type == docType.firstLocation){
+                        locationM.updateOtherMarker(msg);
+                        if(locationM.getMyLocation()){
+                            var _msg = locationM.getMyLocation();
+                            _msg.type = docType.updateLocation;
+                            skywayM.sendMessage(_msg,peer);
+                        }
+                    }else if(msg.type == docType.updateLocation){
+                        locationM.updateOtherMarker(msg);
+                    }
 
-        $('#startTalk').on('click',function(e){
-            e.preventDefault();
-            SkywayM.startTalk();
-        });
+                });
 
-        $('#stopTalk').on('click',function(e){
-            e.preventDefault();
-            SkywayM.stopTalk();
+                locationM.updateMyMarker(function(myLocation){
+                    myLocation.type = docType.updateLocation;
+                    skywayM.sendMessage(myLocation);
+
+                });
+
+            });
+
         });
 
     });
-*/
+
+    $('.zoom').on('click', function(){
+        if($(this).hasClass('zoom-in')){
+            locationM.zoomIn();
+        } else {
+            locationM.zoomOut();
+        }
+    });
+
+    $('input').on('input', function(){
+        $('#type')[0].currentTime = 0;
+        $('#type')[0].play();
+
+    });
+
+    $('input').on('keyup',function(){
+
+        var c = this.selectionStart,
+            r = /[^a-zA-Z0-9]/gi,
+            v = $(this).val();
+        if(r.test(v)) {
+            $(this).val(v.replace(r, ''));
+            c--;
+        }
+        this.setSelectionRange(c, c);
+    });
+
+    $('#theme')[0].play();
 
 });
